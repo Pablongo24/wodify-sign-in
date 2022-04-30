cfcb5ec9b5d848be44766b616345c9af5a7f1847
pablo.vega-behar <pablo.vega-behar@sparksandhoney.com>
2022-04-30 00:41:03 -0400

    Generalizing code

--- auto_reserve/wodify_reserve.py
+++ auto_reserve/wodify_reserve.py
@@ -14,36 +14,63 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
 
 
 class WodifyScraper:
+    """Scrape the Wodify Web App.
+
+    Attributes
+    ----------
+    login_page : str
+        Wodify App URL
+    calendar_page : str
+        URL for the Wodify calendar. This page contains the class schedule.
+    driver : ChromeDriver object
+        Selenium ChromeDriver
+        # TODO: change the executable path to run in Github Actions runner.
+
+    """
     def __init__(self):
         self.login_page = 'https://app.wodify.com/SignIn/Login?OriginalURL=&RequiresConfirm=false'
         self.calendar_page = 'https://app.wodify.com/Schedule/CalendarListViewEntry.aspx'
         self.driver = webdriver.Chrome(executable_path='/Users/pablo.vega-behar/Desktop/chromedriver')
 
     def login(self):
+        """Log-in to the web app.
+
+        Uses username and password stored as secrets.
+        """
         username_element = self.driver.find_element(by=By.ID, value="Input_UserName")
         password_element = self.driver.find_element(by=By.ID, value="Input_Password")
 
-        username_element.send_keys(USERNAME)
-        password_element.send_keys(PASSWORD)
+        username_element.send_keys(os.environ.get("USERNAME"))
+        password_element.send_keys(os.environ.get("PASSWORD"))
 
         login_elem = self.driver.find_element(by=By.CLASS_NAME, value='signin-btn')
         login_elem.click()
 
     def switch_to_calendar(self):
+        """Change from the landing page after logging in to the Calendar page."""
         calendar_elem_id = "AthleteTheme_wtLayoutNormal_block_wtMenu_AthleteTheme_wt67_block_wt37"
         calendar_elem = self.driver.find_element(by=By.ID, value=calendar_elem_id)
         calendar_elem.click()
 
     def change_date_field(self, time_delta=5):
+        """Select the date for class sign up.
+
+        When you arrive in the calendar page, the default view is today's date.
+        CrossFit Union Square's class sign-up opens 5 days in advance.
+        Hence, `time_delta`=5
+
+        Parameters
+        ----------
+        time_delta : int
+            Number of days from today's date to jump forward in the calendar.
+        """
         date_elem_id = 'AthleteTheme_wt6_block_wtMainContent_wt9_W_Utils_UI_wt216_block_wtDateInputFrom'
         date_elem = self.driver.find_element(by=By.ID, value=date_elem_id)
         input_date = datetime.today() + timedelta(time_delta)
@@ -60,13 +87,13 @@ class WodifyScraper:
             If str: expected format '6:00 AM'
         """
         soup = BeautifulSoup(self.driver.page_source, features='html.parser')
-        span = soup.find('span', attrs={'title': 'Open Gym: 8:00 AM - 4:00 PM'})
+        span = soup.find('span', attrs={'title': class_time})
         tr = span.parent.parent.parent
         reserve_link = tr.find('a', attrs={'href': '#'})
         reserve_elem = self.driver.find_element(by=By.ID, value=reserve_link.attrs['id'])
         reserve_elem.click()
 
-    def reserve(self, time_delta=5):
+    def reserve(self, time_delta=5, class_to_reserve='WOD 6:00 AM'):
         self.driver.get(self.login_page)
         self.driver.implicitly_wait(2)
         self.login()
@@ -75,9 +102,19 @@ class WodifyScraper:
         self.driver.implicitly_wait(1)
         self.change_date_field(time_delta=time_delta)
         self.driver.implicitly_wait(1)
-        self.reserve_class()
+        self.reserve_class(class_time=class_to_reserve)
 
 
 if __name__ == '__main__':
+    DEBUGGING = os.environ.get("DEBUGGING")
+    SUNDAY_DAY_OF_WEEK = 6  # Sunday is index 6 in `weekday()`
+
     wodify = WodifyScraper()
-    wodify.reserve(time_delta=2)
+
+    if DEBUGGING:
+        today_day_of_week = datetime.today().weekday()
+        days_delta = SUNDAY_DAY_OF_WEEK - today_day_of_week
+        CLASS_TO_RESERVE = 'Open Gym: 8:00 AM - 4:00 PM'
+        wodify.reserve(time_delta=days_delta, class_to_reserve=CLASS_TO_RESERVE)
+    else:
+        wodify.reserve()
