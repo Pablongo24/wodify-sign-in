7fde1c9d22943b4b46964561becda2d61a33ce2c
pablo.vega-behar <pablo.vega-behar@sparksandhoney.com>
2022-04-30 17:29:35 -0400

    deleted pytest fixture no longer needed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,10 @@
-import os
-
 # noinspection PyPackageRequirements
 import pytest
-from dotenv import load_dotenv
 
 from auto_reserve import WodifyScraper
 
 
 @pytest.fixture()
-def wodify(chromedriver_path):
+def wodify():
     wodify_instance = WodifyScraper()
     return wodify_instance
-
-
-@pytest.fixture()
-def chromedriver_path():
-    load_dotenv()
-    path = os.environ.get('CHROMEDRIVER_EXECUTABLE')
-    return path
